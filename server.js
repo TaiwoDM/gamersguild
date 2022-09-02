5cddd68eb66dac2a366a0603b2797c60f3d11350
Daniel <danielmayowataiwo@gmail.com>
2022-09-02 16:29:11 +0100

    adding procfile for heroku

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ import app from './app.js';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server has started on port ${port}`);
 });
